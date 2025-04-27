c2df57f3bc4fc4c7de454297a6027c01c28e9af0
avanish_sandilya <bhanusandilya74@gmail.com>
2025-04-27 16:24:01 +0530

    adding bitwise operator examples

---
+++ Operators_and_Expressions/bitwise_operators/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        //left shift will double the number
+        System.out.println(5<<1);
+        //right shift will half the number
+        System.out.println(20>>1);
+        //not will change the sign and increase value by 1
+        System.out.println(~5);
+        //& and | operation
+        System.out.println(5&6);
+        System.out.println(5|6);
+    }
+}
