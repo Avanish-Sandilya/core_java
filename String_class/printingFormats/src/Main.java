c00226907574e74d0a1d014ec371a10f1d8a9c47
avanish_sandilya <bhanusandilya74@gmail.com>
2025-04-29 22:32:09 +0530

    adding more printf example

--- String_class/printingFormats/src/Main.java
+++ String_class/printingFormats/src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         float num = 11.67f;
         char c='A';
         System.out.printf("%s %d %.2f %c\n",name,val,num,c);
+        System.out.printf("%2$d %1$.2f %2$d\n",num,val);
     }
 }
